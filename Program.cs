9c8368548d2ef5731d9bda55291331c0fd728d81
Santiago Benjamín Irigoyen <irigoyensantiago623@gmail.com>
2026-09-18 14:59:35 -0300

    18/9/2026

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System.Globalization;
+using W03.Components;
+
+// Custom Requirement: Set default culture to Argentina (es-AR) for currency
+var cultureInfo = new CultureInfo("es-AR");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
